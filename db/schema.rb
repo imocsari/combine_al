30c940d4be30a9cd78379c6a6c09f63a611c93c1
Imre Mocsari <imremocsari1@yahoo.co.uk>
2020-06-28 13:01:19 +0100

    changed sqlite to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_06_26_105909) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "audios", force: :cascade do |t|
     t.datetime "start_time"
     t.datetime "end_time"
